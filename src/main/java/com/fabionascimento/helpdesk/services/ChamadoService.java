bb56a51a94df0d49f4b9820e81759b6a8e6c73e0
Fabio Nascimento <fabiogtr@hotmail.com>
2022-12-08 17:07:57 -0300

    Endpoint findById para Chamado

---
+++ src/main/java/com/fabionascimento/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.fabionascimento.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabionascimento.helpdesk.domain.Chamado;
+import com.fabionascimento.helpdesk.repositories.ChamadoRepository;
+import com.fabionascimento.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = chamadoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+}
